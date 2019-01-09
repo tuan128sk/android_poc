b4392d614d74c642d891901aa0070c9236cbdcc4
Korakot Phaearaya <korakop.p@gmail.com>
2019-01-09 09:08:29 +0700

    init

---
+++ app/src/main/java/com/tisco/android_poc/android_poc/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tisco.android_poc.android_poc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
